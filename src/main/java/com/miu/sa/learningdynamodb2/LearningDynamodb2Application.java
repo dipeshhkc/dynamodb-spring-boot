4bb94e02029984a99214cd2b3843edeba725eb35
dipesh <kcdipesh429@gmail.com>
2023-05-11 11:06:56 -0500

    chore: dependencies

---
+++ src/main/java/com/miu/sa/learningdynamodb2/LearningDynamodb2Application.java
@@ -0,0 +1,13 @@
+package com.miu.sa.learningdynamodb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningDynamodb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningDynamodb2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/miu/sa/learningdynamodb2/LearningDynamodb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miu.sa.learningdynamodb2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningDynamodb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
